d305977cf7bd7d506e8c3e77203259c40ff8f830
Paulette Zorrilla <pauletteazb@gmail.com>
2019-02-21 16:59:51 -0500

    Fix for special case
    Found an error with previous code when testing for 70058888. Fixed!
    Remove message for fix code

--- biggestSmaller/biggestRightToLeft.py
+++ biggestSmaller/biggestRightToLeft.py
@@ -1,5 +1,7 @@
 def findBiggestSmallerRightToLeft(num):
+    # Get Digits from number
     digits = [int(d,10) for d in str(num)]
+    # The first two basic cases, the number has one or two digits
     if(len(digits) == 1):
         return num
     if(len(digits) == 2):
@@ -9,34 +11,64 @@ def findBiggestSmallerRightToLeft(num):
             digits[1] = tmp
         return int("".join(map(str, digits))) 
 
-    # Get last digit from number
-    last = len(digits)-1
+    # Get the biggest number
+    ordered = [int(d,10) for d in str(num)]
+    ordered.sort(reverse=True)
+    biggestNum = ordered[0]
+
+    i = len(digits) - 1
     pivot = -1
-    while(last > 1):
-        current = digits[last]
-        for i in range(last-1, -1, -1):
-            if(digits[i] > current):
-                tmp = digits[i]
-                digits[i] = current
-                digits[last] = tmp
-                pivot = i
-                last = 0
-                break
-        last -= 1
-    if(pivot > 0):
-        result = []
-        for i in range(0, pivot+1):
-            tmp = digits.pop(0)
-            result.append(tmp)
-        digits.sort(reverse=True)
-        result += digits
-        return int("".join(map(str, result)))    
-    else:
-        return pivot
-   
-num = 809179
-#num = 11230
-#num = 37153
+    while (i > 0 and pivot < 0):
+        current = digits[i]
+        if(current < biggestNum):
+            j = i - 1
+            while(j >= 0 and pivot < 0):
+                if(digits[j] > current):
+                    tmp = digits[j]
+                    digits[j] = current
+                    digits[i] = tmp
+                    pivot = j
+                j -= 1
+        i -= 1
+
+    result = 0
+    j = len(digits) - 1
+    for i in range(0, pivot+1):
+        d = digits.pop(0)
+        result += pow(10, j) * d
+        j -= 1
+
+    digits.sort(reverse=True)
+    for d in digits:
+        result += pow(10, j) * d
+        j -= 1
+    return result
+
+# TESTING #
+num = 89
 print "Input: ", num
 output = findBiggestSmallerRightToLeft(num)
 print "Result is: ", output
+
+num = 65
+print "Input: ", num
+output = findBiggestSmallerRightToLeft(num)
+print "Result is: ", output
+
+num = 809179
+print "Input: ", num
+output = findBiggestSmallerRightToLeft(num)
+print "Result is: ", output
+
+num = 71805058
+print "Input: ", num
+output = findBiggestSmallerRightToLeft(num)
+print "Result is: ", output
+
+num = 70058888
+print "Input: ", num
+output = findBiggestSmallerRightToLeft(num)
+print "Result is: ", output
+
+#num = 11230
+#num = 37153
